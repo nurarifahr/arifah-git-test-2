effcaac9ed451e6204a935670be1f396a3bbd14d
nurarifahr <2020303554@mhs.udb.ac.id>
2022-01-25 14:25:06 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hello world !</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt magnam est veniam excepturi eos maxime sunt doloremque incidunt a pariatur, harum animi atque vel sit nam quam, repudiandae officiis itaque.</p>
+    <ul>
+        <li>Arifah</li>
+    </ul>
 </body>
 </html>
